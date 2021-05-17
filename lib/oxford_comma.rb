aff96e5f2cc2b4e5d24bb188252601b50c929cb7
Zach Frohardt <zfrohardt@gmail.com>
2021-05-17 11:39:34 -0700

    commit tree 51c1d4ca3e5980cdf8a12a9df8c99bbda0f35425

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        return array.first
+    elsif array.length == 2
+        return array.join(" and ")
+    else
+        last = array.pop
+        string = array.join(", ")
+        return "#{string}, and #{last}"
+    end
 end
